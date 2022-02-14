af1bef6a9a90f1df6ba4cff70ed4882f223f5fc7
Nathan Smith <nsmith@lcs.on.ca>
2022-02-14 17:13:43 -0500

    check answer button abstracted

---
+++ ArithmeticAce/Helpers/AnimationsAbstractionView.swift
@@ -0,0 +1,20 @@
+//
+//  AnimationsAbstractionView.swift
+//  ArithmeticAce
+//
+//  Created by Nathan Smith on 2022-02-14.
+//
+
+import SwiftUI
+
+struct AnimationsAbstractionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnimationsAbstractionView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimationsAbstractionView()
+    }
+}
