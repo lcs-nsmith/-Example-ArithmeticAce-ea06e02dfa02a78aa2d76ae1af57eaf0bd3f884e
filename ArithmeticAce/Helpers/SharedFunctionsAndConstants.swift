2e600053551364faefaa75d20ed81230dae17ddb
Nathan Smith <nsmith@lcs.on.ca>
2022-03-04 13:07:34 -0500

    added a shard function that provides he location at which a file can be saved

---
+++ ArithmeticAce/Helpers/SharedFunctionsAndConstants.swift
@@ -0,0 +1,17 @@
+//
+//  SharedFunctionsAndConstants.swift
+//  ArithmeticAce
+//
+//  Created by Nathan Smith on 2022-02-26.
+//
+
+import Foundation
+
+// Return the directory that we can save user data in
+func getDocumentsDirectory() -> URL {
+    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    return paths[0]
+}
+
+// Define a file label (or file name) that we will write to within that directory
+let savedFavouritesLabel = "savedFavourites"
